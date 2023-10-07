099d9406f5bd89bd124b94922fd4b9234986e277
Svyazist <sviazist.vsa@gmail.com>
2023-10-07 19:36:30 +0500

    feat: not working button script not done

--- js/scripts/button.js
+++ js/scripts/button.js
@@ -3,18 +3,13 @@
 let nav = document.querySelector(".mmButton"),
     cont = document.querySelector(".cButton");
 
-document.addEventListener("click", tog );    
-
-
-function tog (event) {
-        
-    let target = event.target;
-
-    if(target.classList.contains("mmButton")) nav.classList.add("displayOn");
-    if(target.classList.contains("cButton")) cont.classList.add("displayOn");
-    if(!target.classList.contains("mmButton") && target.classList.contains("cButton")) {
-        nav.classList.remove("displayOn");
-        cont.classList.remove("displayOn");
-    }
     
-}
+
+
+
+
+    document.onclick = (e) => {if(e.target ==nav) nav.classList.add("displayOn");}
+   
+    document.onclick = (e) => {if (e.target != nav) nav.classList.remove("displayOn");}
+            
+    document.onclick = (e) => {if (e.target != cont) cont.classList.remove("displayOn");}    
