5cee4aadd6e4badcc29571837ccd62f840320c1a
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-11-18 17:50:07 +0500

    feat: description function start

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 "use strikt"
 
 export {togButton} from "./scripts/button.js";
+export {showDescription, description} from "./scripts/Sbutton.js";
---
+++ js/scripts/Sbotton.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function showDescription(){
+  
+}
+
+let descriptions = {
+  one: "This game is written on the phone without the use of package collectors, while working as a locksmith. And it requires optimization, which I will do later.", 
+};
+
+export { showDescription, description};
