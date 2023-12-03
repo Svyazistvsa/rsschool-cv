d256bac7fd4b25c7cb9e4bc7d5b5e2a8b2a97c19
Svyazistvsa <Sviazist.vsa@gmail.com>
2023-12-03 19:26:02 +0500

    feat: experements anf errors

--- js/scripts/Sbutton.js
+++ js/scripts/Sbutton.js
@@ -10,7 +10,7 @@ function showDescription(e){
      }
    }
   }
-}
+
 
 let descriptions = {
   one: "This game is written on the phone without the use of package collectors, while working as a locksmith. And it requires optimization, which I will do later.", 
