16d01562680ec479eb0665d03ef6e80e61bfe09c
Svyazist <sviazist.vsa@gmail.com>
2023-12-20 22:58:33 +0500

    feat: start description button function

---
+++ js/scripts/description.js
@@ -0,0 +1,7 @@
+"use strict"
+
+export function descFunc (event){
+    if(event.target.classList.f){
+        //подумать как одним общим обработчиком событий обработать все кнопки description
+    }
+}
